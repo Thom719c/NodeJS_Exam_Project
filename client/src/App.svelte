4ff38615a3edef4a428e61bb1a40981f9a9310d7
Thom <thom719c@stud.kea.dk>
2023-05-04 19:48:53 +0200

    Initial commit

---
+++ client/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+<main>
+  <h1>Gaming Oasis</h1>
+  <h1>StreamSpot: All-in-One Gaming & Chat</h1>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+</style>
