c2ef16b2f0c4ca25c99448f7d7cf7a4334f85f18
Thom <thom719c@stud.kea.dk>
2023-05-28 02:36:56 +0200

    Update Game.svelte

--- client/src/pages/Game/Game.svelte
+++ client/src/pages/Game/Game.svelte
@@ -1,19 +1,30 @@
 <script>
     import { onMount } from "svelte";
+    import { useNavigate } from "svelte-navigator";
     import { selectedGame } from "../../stores/stores.js";
     import Game from "../../components/Game/Game.svelte";
 
     let game;
+    const navigate = useNavigate();
 
     onMount(() => {
         game = $selectedGame;
         console.log(game);
     });
+
+    const goBackToProfile = () => {
+        navigate("/games");
+    };
 </script>
 
 <div class="container game-box">
     {#if game}
-        <h1>{game.name}</h1>
+        <div class="header my-2">
+            <button class="back-button" on:click={goBackToProfile}>
+                Go Back
+            </button>
+            <h1>{game.name}</h1>
+        </div>
         <Game src={game.src} />
     {/if}
 </div>
@@ -32,5 +43,24 @@
         box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
         border-radius: 15px;
         border: 1px solid #ccc;
+    }
+
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .back-button {
+        background-color: #67c2dd41;
+        border-color: #e5e047;
+        font-weight: bold;
+        font-size: 14px;
+        padding: 6px 12px;
+        letter-spacing: 1px;
+    }
+
+    .back-button:hover {
+        background-color: rgba(71, 135, 155, 0.255);
     }
 </style>
