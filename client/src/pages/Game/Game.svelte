86ec3128cb2fa22572418c543a3e897c260ca644
Thom <thom719c@stud.kea.dk>
2023-05-28 02:30:39 +0200

    Updated games

--- client/src/pages/Game/Game.svelte
+++ client/src/pages/Game/Game.svelte
@@ -11,7 +11,7 @@
     });
 </script>
 
-<div class="container content-box">
+<div class="container game-box">
     {#if game}
         <h1>{game.name}</h1>
         <Game src={game.src} />
@@ -21,5 +21,16 @@
 <style>
     .container {
         flex: 1;
+        max-width: 960px;
+        max-height: 600px;
+    }
+
+    .game-box {
+        margin-top: 75px;
+        background: rgba(48, 76, 96, 0.9);
+        text-align: center;
+        box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+        border-radius: 15px;
+        border: 1px solid #ccc;
     }
 </style>
--- client/src/pages/Game/Games.svelte
+++ client/src/pages/Game/Games.svelte
@@ -1,62 +1,81 @@
-<!-- <script>
-    import Game from "../../components/Game/Game.svelte";
-  
-    const games = [
-      { name: "Football", image: "", src: "src/assets/unityGames/football/footballGame.html" },
-      { name: "M2 Game", image: "", src: "src/assets/unityGames/m2Game/m2Game.html" },
-      // Add more games to the array
-    ];
-  
-    let selectedGame;
-  
-    function selectGame(game) {
-      selectedGame = game;
-    }
-  </script>
-  
-  <div>
-    {#each games as game}
-      <button on:click={() => selectGame(game)}>
-        {game.name}
-      </button>
-    {/each}
-  </div>
-  
-  {#if selectedGame}
-    <Game src={selectedGame.src} />
-  {/if}
-   -->
 <script>
-    import { selectedGame } from "../../stores/stores.js";
-    import { useNavigate } from "svelte-navigator";
+  import { selectedGame } from "../../stores/stores.js";
+  import { useNavigate } from "svelte-navigator";
+  import footballImage from "../../assets/unityGames/football/footballImage.png";
+  import m2Image from "../../assets/unityGames/m2Game/m2Image.png";
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const games = [
-        {
-            id: 1,
-            name: "Football",
-            image: "",
-            src: "/src/assets/unityGames/football/footballGame.html",
-        },
-        {
-            id: 2,
-            name: "M2 Game",
-            image: "",
-            src: "/src/assets/unityGames/m2Game/m2Game.html",
-        },
-    ];
+  const games = [
+    {
+      id: 1,
+      name: "Football",
+      image: footballImage,
+      src: "/src/assets/unityGames/football/footballGame.html",
+    },
+    {
+      id: 2,
+      name: "M2",
+      image: m2Image,
+      src: "/src/assets/unityGames/m2Game/m2Game.html",
+    },
+  ];
 
-    function selectGame(game) {
-        selectedGame.set(game);
-        navigate(`/games/${game.id}`);
-    }
+  function selectGame(game) {
+    selectedGame.set(game);
+    navigate(`/games/${game.id}`);
+  }
 </script>
 
-<div>
-    {#each games as game}
-        <button on:click={() => selectGame(game)}>
-            {game.name}
-        </button>
-    {/each}
+<div class="game-container mt-4">
+  {#each games as game}
+    <button class="game" on:click={() => selectGame(game)}>
+      <div class="game-card">
+        <img
+          class="game-image"
+          src={game.image}
+          alt={`Cover image for ${game.name}`}
+        />
+        <p class="game-name text-gradient">{game.name}</p>
+      </div>
+    </button>
+  {/each}
 </div>
+
+<style>
+  .game-container {
+    list-style-type: none;
+    padding: 0;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  .game {
+    background-color: rgba(48, 76, 96, 0.9);
+    color: #c6d4df;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    margin: 10px;
+    padding: 10px;
+    width: 300px;
+    height: 200px;
+    transition: transform 0.2s ease-in-out;
+  }
+
+  .game:hover {
+    transform: translateY(-5px);
+  }
+
+  .game-image {
+    width: 250px;
+    height: 150px;
+    margin: 0 10px;
+    border-radius: 5px;
+  }
+
+  .game-name {
+    font-size: 18px;
+    text-align: center;
+  }
+</style>
